16639bf19f4243fb09a1e642adce04a47855c109
Henry Quinn <henryquinniv@gmail.com>
2022-01-14 10:54:45 -0500

    Adding events page.

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,9 @@
 import * as React from "react"
 import { Link } from "gatsby"
 import { StaticImage } from "gatsby-plugin-image"
+import styles from "./index.module.css"
 
-import Layout from "../components/layout"
+import Layout from "../components/Layout"
 import Seo from "../components/seo"
 
 const IndexPage = () => (
@@ -12,7 +13,7 @@ const IndexPage = () => (
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
     <StaticImage
-      src="../images/gatsby-astronaut.png"
+      src="../images/mayday-logo-circle.png"
       width={300}
       quality={95}
       formats={["auto", "webp", "avif"]}
@@ -22,14 +23,13 @@ const IndexPage = () => (
     <p>
       <Link to="/page-2/">Go to page 2</Link> <br />
       <Link to="/using-typescript/">Go to "Using TypeScript"</Link> <br />
-      <Link to="/using-ssr/">Go to "Using SSR"</Link> <br />
       <Link to="/blog">Go to "Blog"</Link> <br />
       <Link to="/contact">Go to "Contact"</Link> <br />
       <Link to="/donate">Go to "Donate"</Link> <br />
+      <Link to="/events">Go to "Events"</Link> <br />
       <Link to="/faq">Go to "FAQ"</Link> <br />
       <Link to="/get-involved">Go to "Get Involved"</Link> <br />
-      <Link to="/info">Go to "Info"</Link> <br />
-      <Link to="/using-dsg">Go to "Using DSG"</Link>
+      <Link to="/info">Go to "Info"</Link>
     </p>
   </Layout>
 )
