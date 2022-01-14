d9dfce9e195d2ed816cb1b47d2413a112aceddf6
Henry Quinn <henryquinniv@gmail.com>
2022-01-14 10:57:48 -0500

    Working on Gatsby plumbing.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,14 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
-    siteUrl: `https://gatsbystarterdefaultsource.gatsbyjs.io/`,
+    author: `@MayDayStrike`,
+    description: `May Day is a general labor strike beginning on the 1st of May and lasting #UntilDemandsAreMet.`,
+    keywords: `May, Day, Strike, Labor, Unions`,
+    title: `May Day Strike`,
+    siteUrl: `https://maydaystrike.org/`,
+    social: {
+      github: "MayDayStrike",
+      twitter: "MayDayStrike22",
+    }
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -11,26 +16,50 @@ module.exports = {
     {
       resolve: `gatsby-source-filesystem`,
       options: {
-        name: `images`,
-        path: `${__dirname}/src/images`,
+        name: `src`,
+        path: `${__dirname}/src/`,
       },
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     {
-      resolve: `gatsby-plugin-manifest`,
+      resolve: `gatsby-transformer-remark`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
-        start_url: `/`,
-        background_color: `#663399`,
-        // This will impact how browsers show your PWA/website
-        // https://css-tricks.com/meta-theme-color-and-trickery/
-        // theme_color: `#663399`,
-        display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
+        plugins: [
+          {
+            resolve: `gatsby-remark-images`,
+            options: {
+              maxWidth: 300,
+            },
+          },
+          `gatsby-remark-autolink-headers`,
+          `gatsby-remark-copy-linked-files`,
+        ],
       },
     },
+    `gatsby-remark-copy-linked-files`,
+    `gatsby-remark-smartypants`,
+    {
+      resolve: `gatsby-plugin-manifest`,
+      options: {
+        name: `May Day Strike`,
+        short_name: `MDS 2022`,
+        start_url: `/`,
+        background_color: `#C73A26`,
+        theme_color: `#C73A26`,
+        display: `browser`,
+        icon: `src/images/mayday-logo-circle.png`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-react-svg`,
+      options: {
+        rule: {
+          include: /\.inline\.svg$/
+        }
+      }
+    },
+    `gatsby-transformer-yaml`,
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
