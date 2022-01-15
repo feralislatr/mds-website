0f1e786a61647a7f91aa43871c2a4b8756c57077
Henry Quinn <henryquinniv@gmail.com>
2022-01-15 14:36:14 -0500

    Updating branding a bit before we add more styling.

--- src/components/Layout/index.js
+++ src/components/Layout/index.js
@@ -39,9 +39,9 @@
              marginTop: `2rem`,
            }}
          >
-           © {new Date().getFullYear()}, Built with
+           © {new Date().getFullYear()},
            {` `}
-           <a href="https://www.gatsbyjs.com">Gatsby</a>
+           <a href="https://twitter.com/maydaystrike22">May Day Strike</a>
          </footer>
        </div>
      </>
