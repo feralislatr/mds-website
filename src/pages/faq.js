5ab31439c7b9040b306995825ee292758328e711
Henry Quinn <henryquinniv@gmail.com>
2022-01-15 14:36:53 -0500

    First step in moving FAQ over from Reddit.

--- src/pages/faq.js
+++ src/pages/faq.js
@@ -7,8 +7,103 @@ import Seo from "../components/seo"
 const FAQPage = () => (
   <Layout>
     <Seo title="Page two" />
-    <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
+    <h1>May Day Strike FAQ</h1>
+    <ol>
+      <li>Why is a strike an effective tool against worker oppression?</li>
+        <ul>
+          <li>Striking puts financial pressure on employers, forcing them to acknowledge employee demands.</li>
+          <li>Striking draws a great deal of public attention to the needs of workers.</li>
+          <li>Striking demonstrates workers’ commitment to change.</li>
+        </ul>
+        <p>Read about the strike that resulted in 8-hour workdays becoming the norm here (this is only one example of a major, successful strike movement): <a href="https://www.encyclopedia.com/history/encyclopedias-almanacs-transcripts-and-maps/eight-hour-day-movement" target="_blank">https://www.encyclopedia.com/history/encyclopedias-almanacs-transcripts-and-maps/eight-hour-day-movement</a></p>
+
+      <li>When is the May Day Strike?</li>
+        <p>According to your local time zone, the May Day Strike begins at midnight on May 1st, 2022. It is strongly encouraged for anyone who is able to strike indefinitely to do so until demands have been met.</p>
+
+      <li>Why an indefinite strike?</li>
+        <p>According to various political sources, an inactive workforce would cripple the economy. We aim to do just that - bring corporations and employers to a point of discomfort that forces them to acknowledge our demands. The May Day Strike has no pre-determined end date. The strike ends when our demands have been met.</p>
+
+      <li>Why May 1st?</li>
+        <ul>
+          <li>International Workers' Day - May 1st is International Workers' Day (Labor Day). Learn the history of May Day here: <a href="https://www.britannica.com/topic/May-Day-international-observance" target="_blank">https://www.britannica.com/topic/May-Day-international-observance</a></li>
+          <li>Preparation - A strike lasting ten days or longer will inevitably require financial preparation for all participants. Such advance notice provides ample time to accrue savings, stockpile food, etc.</li>
+          <li>Allowing time for changes in employer policy - For employers who are currently unaware of the need for reform, this period of 3+ months will allow time for wage and benefit adjustments. As the movement gains momentum, it is likely some employers will begin to take preemptive measures such as improving work conditions and wages.</li>
+          <li>Time for Organization and planning - As a grassroots movement, the May Day Strike will take time to organize on an international level. Between this moment and May 1st, we will have ample time to plan and prepare the movement for what comes next.</li>
+        </ul>
+
+      <li>Where is the strike taking place?</li>
+        <p>The May Day Strike is an international movement and is therefore not limited to any specific country or region. We encourage participation globally, regardless of location.</p>
+
+      <li>How do I participate?</li>
+        <p>On May 1st, 2022, we are calling for all participants to call off work, or to walk off, and to remain away from work. Ideally, all participating workers should be prepared to remain away from work indefinitely until our collective demands have been met.</p>
+
+      <li>What are the demands of this movement?</li>
+        <ul>
+          <li>A living wage that regularly adjusts for inflation and mandatory overtime pay.</li>
+          <li>Paid time off for vacations, medical leave, bereavement, and parental leave.</li>
+          <li>Increased union protections and universal right to unionize.</li>
+          <li>A universal, single-payer healthcare system.</li>
+          <li>Student and medical debt forgiveness for the working class.​</li>
+        </ul>
+
+      <li>What about other crucial demands not listed here?</li>
+        <p>Not to worry! The May Day Strike will be an ongoing movement. Further demands will be specified as the movement evolves beyond the initial strike.</p>
+
+      <li>What can I do to help?</li>
+        <ul>
+          <li>The easiest way to help the movement is by raising awareness of the May Day Strike via social media! - All social media outlets are acceptable. We suggest including one of our approved posters (coming soon!) for immediate visual engagement and pairing it with a link to the <a href="https://reddit.com/r/MayDayStrike" target="_blank">/r/MayDayStrike</a> subreddit/website/Discord. We strongly advise directing anyone with questions about the movement to our FAQ pages.</li>
+          <li>Connect with your local unions, or the IWW - <a href="https://www.iww.org/" target="_blank">https://www.iww.org/</a></li>
+          <li>Fill out our brief volunteer form - <a href="https://docs.google.com/forms/d/e/1FAIpQLSeNzjMZdnnp8VC4_kPsSOGbXOcZ3LkOTWNm_RaWZKRwIN5E_g/viewform" target="_blank">https://docs.google.com/forms/d/e/1FAIpQLSeNzjMZdnnp8VC4_kPsSOGbXOcZ3LkOTWNm_RaWZKRwIN5E_g/viewform</a> *International Form Coming soon!*</li>
+          <li>Coordinate a local movement in your workplace or city - Speak discretely with your coworkers about the May Day Strike and invite them to become part of the community!</li>
+        </ul>
+
+        <p>We will also need volunteers who can provide the following:</p>
+        <ul>
+          <li>Volunteer legal counsel (supporting strike developers, detained picketers, etc.)</li>
+          <li>Food and financial assistance to strikers (working with food banks, setting up food distribution centers for strikers, etc.)</li>
+          <li>Providing transportation or bus passes</li>
+          <li>Additional needs, to be announced in the coming weeks</li>
+        </ul>
+
+      <li>Why do I have to use the approved strike materials?</li>
+        <p>In order to present a unified front for this movement, our message, demands, and neutral political stance must be maintained. As the movement continues to grow, there will be increasing amounts of acknowledgment and coverage.</p>
+
+      <li>How can I contact other participants?</li>
+        <p>The May Day Strike lounge is always open for suggestions and discussions. We also welcome you to join our Discord! <a href="https://discord.gg/UC6zqyeep2" target="_blank">https://discord.gg/UC6zqyeep2</a></p>
+
+      <li>Should I speak to influencers, media outlets, and other major entities about the May Day Strike?</li>
+        <p>We ask that participants refrain from making any contact with major personalities or entities regarding the strike for the time being. Once the number of willing participants has reached a certain level, the development team will contact media outlets and influencers to provide official and thoroughly curated statements. If any media outlets or influencers approach you with questions about the May Day Strike, please do not answer any questions directly or give any interviews. Instead, please direct them to this subreddit, or one of our official social media channels.</p>
+
+
+      <li>What can I expect during the strike?</li>
+        <p>Organizers will be reaching out to the media during the strike and communicating with participants via Reddit, Discord and other social media outlets. It is strongly recommended to join and familiarize yourself with the May Day Strike community well before the first day of the strike.</p>
+
+      <li>How do we support ourselves during the strike?</li>
+        <p>Planning and preparing for the strike during the coming months will be crucial! During the next few months, please consider the following preparation steps, as well as any others that fit your needs and lifestyle:</p>
+        <ul>
+          <li>Paying bills ahead of time</li>
+          <li>Stocking up on non-perishable grocery essentials</li>
+          <li>Identifying local pantries and other sources of aid</li>
+          <li>Connecting with unions and networks of mutual aid in your area</li>
+        </ul>
+
+      <li>Will there be protests involved?</li>
+        <p>As with any movement, some protests will inevitably occur. If you are considering holding a local protest during the May Day Strike, please adhere to all local laws. Best practices for picketing are detailed here: <a href="https://www.law.nyu.edu/centers/race-inequality-law/protest-tips" target="_blank">https://www.law.nyu.edu/centers/race-inequality-law/protest-tips</a></p>
+
+      <li>What are the risks of participating?</li>
+        <p>Potential loss of income and job loss are among the top risks of participating in any strike. This risk is lessened significantly at higher levels of strike participation! We strongly encourage you to include coworkers, and in the case of franchise workers, to reach out to employees at similar franchise locations in your area.</p>
+
+      <li>What should I do if I’m in a union that doesn’t allow me to strike?</li>
+        <p>Our team is currently collaborating with other movements to find ways for everyone to participate in some way, regardless of union status. We ask for your patience as we navigate the legal aspects of this strike.</p>
+
+      <li>Is this a Left or a Right movement?</li>
+        <p>Neither. The May Day Strike is a Top vs Bottom movement. We do not align ourselves with any political group, and we ask all participants to respect this in their communications with each other and with staff.</p>
+
+      <li>Does the May Day Strike involve or endorse any extremist or violent action?</li>
+        <p>Not under any circumstances.</p>
+    </ol>
+
+    <p>This FAQ list is continually evolving and non-exhaustive. Updates will be added as more questions are brought to our attention. Thank you for standing in solidarity with us!</p>
     <Link to="/">Go back to the homepage</Link>
   </Layout>
 )
